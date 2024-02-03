cfe6d689a86f4fa5e9a3b22289b66c079bf3e4b7
Muhammad <Muhammad@DESKTOP-SVRE260>
2024-02-03 17:12:01 +0500

    Add project files.

---
+++ DeleteMethodviaSystem.net.http/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+
+public class DeleteMethod
+{
+    static async Task Main(string[] args)
+    {
+        using (var client = new HttpClient()) 
+        {
+            HttpResponseMessage response = await client.DeleteAsync("https://jsonplaceholder.typicode.com/posts/1");
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"Successfully deleted!!! {response.StatusCode}");
+            }
+
+            else
+            {
+                Console.WriteLine($"Error has occured: {response.StatusCode}");
+            }
+        }
+    }
+}
